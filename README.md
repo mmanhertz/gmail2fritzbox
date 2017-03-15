63202c14070ca597c99577311490b93b297f60ba
mmanhertz <matthias.manhertz@seerene.com>
2017-03-15 20:03:05 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # gmail2fritzbox
-Convert contacts exported from gmail as .csv to an .xml that can be imported as phonebook on a FritzBox 6360 Cable
+
+Convert contacts exported from gmail as .csv to an .xml that can be imported as
+phonebook on a FritzBox 6360 Cable.
+
+The Fritzbox has a Gmail Sync function, but because gmail always adds the
+country code in front of all phone numbers, there is no way to not have `+49`
+(for Germany in my case) at the beginning of every number in the fritzbox
+phonebook.
+
+In my case this leads to the connected fritz phones not showing the names of
+callers, because the incoming numbers do not have the `+49` prefix and therefore
+are not matched to the phone book entries.
+
+This is my workaround.
+
+It uses only the `Name` and the 4 `Phone...` fields from the gmail .csv.
+Everything else is discarded. The resulting .xml is not formatted exceptionally
+well. The `+49` prefix is not replaced with a `0`, which may look weird, but it
+works for my usecase.
+
+Feel free to use and this as you want, but do so at your own risk.
+
+I have not added any error handling (nor do plan to do so) and the output file
+is overridden without asking, if it existed previously.
---
+++ gmail2fritzbox.py
@@ -0,0 +1,132 @@
+"""
+    This module converts contacts exported from gmail as .csv to an .xml that
+    can be imported as phonebook on a FritzBox 6360 Cable.
+
+    Author: Matthias Manhertz
+    License: MIT
+    Date: 15.03.2017
+"""
+
+import argparse
+import re
+
+import pandas as pd
+
+
+XML_TEMPLATE = '''
+<?xml version="1.0" encoding="utf-8"?>
+<phonebooks>
+    <phonebook>
+        {contacts}
+    </phonebook>
+</phonebooks>
+'''
+
+CONTACT_TEMPLATE = '''
+<contact>
+    <category />
+    <person>
+        <realName>{name}</realName>
+    </person>
+    {telephony}
+    <services />
+    <setup />
+    <uniqueid>{id}</uniqueid>
+</contact>
+'''
+
+TELEPHONY_TEMPLATE = '''
+<telephony nid="{num_count}">
+    {numbers}
+</telephony>
+'''
+
+NUMBER_TEMPLATE = '''
+<number type="{type}" prio="{prio}" id="{id}">{number}</number>
+'''
+
+
+def gmail2fritzbox(in_path, out_path):
+    df = pd.read_csv(in_path, sep=',', header=0)
+
+    relevant_headers = ['Name']
+    relevant_headers.extend([col for col in list(df) if 'Phone' in col])
+
+    xml_contacts = []
+    for contact in df[relevant_headers].itertuples():
+        xml_contacts.append(contact2xml(contact))
+
+    xml = XML_TEMPLATE.format(contacts='\n'.join(xml_contacts))
+
+    with open(out_path, 'w') as f:
+        f.write(xml)
+
+
+def contact2xml(entry):
+    numbers = []
+    id, name = entry[0:2]
+    for i in range(4):
+        numbers.append(entry[(i + 1) * 2:(i + 2) * 2])
+    return CONTACT_TEMPLATE.format(
+        name=name,
+        telephony=numbers2xml(numbers),
+        id=id,
+    )
+
+
+def numbers2xml(numbers):
+    num_count = 0
+    xml_numbers = []
+    numbers = clean_numbers(numbers)
+
+    for ntype, number in numbers:
+        xml_numbers.append(number2xml(number, ntype, num_count))
+        num_count += 1
+
+    return TELEPHONY_TEMPLATE.format(
+        num_count=num_count,
+        numbers='\n'.join(xml_numbers)
+    )
+
+
+def number2xml(number, ntype, id):
+    return NUMBER_TEMPLATE.format(
+        type=ntype,
+        number=number,
+        id=id,
+        prio=1 if id == 0 else 0,
+    )
+
+
+def clean_numbers(numbers):
+    cleaned_numbers = []
+    for ntype, number in numbers:
+        if type(ntype) == str:
+            # For some reason gmail sometimes exports two numbers separated by
+            # three colons. Separate them and treat them as different numbers.
+            split_numbers = number.split(' ::: ')
+            for n in split_numbers:
+                # there can be additional chars and text, remove that stuff
+                n = re.sub('[^0123456789()+]', '', n)
+                # remove the country prefix if any
+                n = rm_prefix('+49', n)
+                cleaned_numbers.append((ntype, n))
+    return cleaned_numbers
+
+
+def rm_prefix(prefix, txt):
+    if txt.startswith(prefix):
+        return txt[len(prefix):]
+    return txt
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('inpath', help='Filepath to the input .csv file.')
+    parser.add_argument('outpath', help='Filepath to the output .xml file.')
+
+    args = parser.parse_args()
+
+    gmail2fritzbox(args.inpath, args.outpath)
+
+
